fd7190e22650ce2bfc6a16ae3c49f023fffd97fe
LuigiMenez <ltc.menez@gmail.com>
2022-06-09 11:17:49 +0200

    Add pattern mvc in the project

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,30 @@
 import express from "express";
 import "reflect-metadata";
 import { Request, Response } from "express";
-import { Employee } from "./models/employee";
 import myConnectDB from "./connectdb";
+import router from "./routes/router";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+const port = parseInt(process.env.PORT ?? "3000", 10);
 
 myConnectDB
   .initialize()
   .then(() => {
     return console.log("Data Source has been initialized ");
   })
-  .catch((err) => {
+  .catch((err: Error) => {
     return console.error("Error during Data Source initialization", err);
   });
 
 const app = express();
 app.use(express.json());
 
-const port = parseInt(process.env.PORT ?? "3000", 10);
+app.use(router);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello");
-});
-
-app.get("/employees", async function (req: Request, res: Response) {
-  const employees = await myConnectDB.getMongoRepository(Employee).find();
-  res.json(employees);
-});
-
-app.post("/employees", async function (req: Request, res: Response) {
-  const employee: any = await myConnectDB
-    .getMongoRepository(Employee)
-    .insertOne(req.body);
-  const results = await myConnectDB.getMongoRepository(Employee).save(employee);
-  return res.send(results);
 });
 
 app.listen(port, () => {
