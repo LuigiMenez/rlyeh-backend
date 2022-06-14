e57e471f8f5bb1d326c2c877a3bf4bb329f991e5
LuigiMenez <ltc.menez@gmail.com>
2022-06-14 10:30:14 +0200

    Add routes find skills and techskills

--- src/controllers/leadDevControllers.ts
+++ src/controllers/leadDevControllers.ts
@@ -3,3 +3,8 @@ import { Employee } from "../models/employee";
 import myConnectDB from "../connectdb";
 
 const db = myConnectDB.getMongoRepository(Employee);
+
+export const employeeSkills = async (req: Request, res: Response) => {
+  const results = await db.findOneBy(req.params.id);
+  return res.send(results?.skills);
+};
