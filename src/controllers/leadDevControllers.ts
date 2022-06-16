ca28422b8ed68c5dbe516f1e908b7ca41325bff7
LuigiMenez <ltc.menez@gmail.com>
2022-06-16 21:43:47 +0200

    Add error management in the route

--- src/controllers/leadDevControllers.ts
+++ src/controllers/leadDevControllers.ts
@@ -5,6 +5,10 @@ import myConnectDB from "../connectdb";
 const db = myConnectDB.getMongoRepository(Employee);
 
 export const employeeSkills = async (req: Request, res: Response) => {
-  const results = await db.findOneBy(req.params.id);
-  return res.send(results?.skills);
+  try {
+    const results = await db.findOneBy(req.params.id);
+    return res.send(results?.skills);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
